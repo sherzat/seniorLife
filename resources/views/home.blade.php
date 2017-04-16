44995114eafbe665249bc9a5b84199ebc49ef771
Efrem Gebremedhin <effer.vision@gmail.com>
2017-04-16 14:07:16 +0200

    survey page modified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,56 +3,44 @@
 @section('content')
 
 <main class="main-class">
-     {{--<section>
-            <article>
-                <p>hello world</p>
-            </article>
-        </section>
+<section class="section-settings">
+    <header style="background-color: teal;height: 70px;color: white;padding-top: 12px;padding-right: 5px"><h2>Overall Result of Survey</h2></header>
 
-        <section>
-            <article>
-                <p>hello library</p>
-            </article>
-        </section>--}}
-    </main>
+    <article>
+        <p style="font-size: 48px; color: black;text-align: center;padding-top: 12px;"> Your Score 78% it is ranked Good</p>
+        <p style="color: black;text-align: center;padding-top: 12px;">For more information click here</p>
+    </article>
+
+</section>
+    <section class="section-settings">
+        <header style="background-color: teal;height: 70px;color: white;padding-top: 12px;padding-right: 5px">
+            <h2>
+            Overall Result of Wristband
+            </h2>
+
+        </header>
+    </section>
+</main>
 
    <aside class="aside-class">
 
        <div class="list-group">
-           <a href="#" class="foo fixsize list-group-item list-group-item-action flex-column align-items-start">
-               {{--<div class="d-flex w-100 justify-content-between">
-                   <h5 class="mb-1">List group item heading</h5>
-                   <small class="text-muted">3 days ago</small>
-               </div>
-               <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-               <small class="text-muted">Donec id elit non mi porta.</small>--}}
+           <a href="#" class="foo aside-settings list-group-item list-group-item-action flex-column align-items-start">
+               <h1 style="text-align: center;color: white; margin-top: 20px">General Overview</h1>
            </a>
 
-           <a href="#" class="foo fixsize list-group-item list-group-item-action flex-column align-items-start">
-               {{--<div class="d-flex w-100 justify-content-between">
-                   <h5 class="mb-1">List group item heading</h5>
-                   <small class="text-muted">3 days ago</small>
-               </div>
-               <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-               <small class="text-muted">Donec id elit non mi porta.</small>--}}
+           <a href="#" class="foo aside-settings list-group-item list-group-item-action flex-column align-items-start">
+               <h1 style="text-align: center;color: white; margin-top: 30px">Survey Results</h1>
            </a>
 
-           <a href="#" class="foo fixsize list-group-item list-group-item-action flex-column align-items-start">
-             {{--<div class="d-flex w-100 justify-content-between">
-                   <h5 class="mb-1">List group item heading</h5>
-                   <small class="text-muted">3 days ago</small>
-               </div>
-               <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-               <small class="text-muted">Donec id elit non mi porta.</small>--}}
+           <a href="#" class="foo aside-settings list-group-item list-group-item-action flex-column align-items-start">
+               <h1 style="text-align: center;color: white; margin-top: 72px">Wristband</h1>
            </a>
 
-           <a href="#" class="foo fixsize list-group-item list-group-item-action flex-column align-items-start">
-          {{-- <div class="d-flex w-100 justify-content-between">
-                   <h5 class="mb-1">List group item heading</h5>
-                   <small class="text-muted">3 days ago</small>
-               </div>
-               <p class="mb-1">Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit.</p>
-               <small class="text-muted">Donec id elit non mi porta.</small>--}}
+           <a href="#" class="foo aside-settings list-group-item list-group-item-action flex-column align-items-start">
+
+              <h1 style="text-align: center;color: white; margin-top: 72px">Share</h1>
+
            </a>
        </div>
    </aside>
